2722d66aefc3e83af60a70270ecf29b82822dd63
frenkdoc <quarantafrancoq@gmail.com>
2021-02-06 14:32:13 +0100

    fixes file

--- prova_pratica/prova_lv_uno/prova_lv_uno/urls.py
+++ prova_pratica/prova_lv_uno/prova_lv_uno/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from app_prova.views import homepage
+from .app_prova.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
